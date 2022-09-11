d328607101f6c2965a1c83323f18c4ec60dac883
cgzmartinez <105771374+cgzmartinez@users.noreply.github.com>
2022-09-11 02:23:09 -0400

    Updated mongoose connection_uri in index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const express = require("express"),
 const Movies = Models.Movie;
 const Users = Models.User;
 
-mongoose.connect("mongodb://localhost:27017/myFlixDB", {
+mongoose.connect(process.env.CONNECTION_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
